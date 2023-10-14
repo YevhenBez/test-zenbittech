fb6901e68e004b61086e9bb2596df23a87f9e02f
YevhenBez <115114239+YevhenBez@users.noreply.github.com>
2023-10-14 15:54:18 +0000

    Deploying to gh-pages from @ YevhenBez/test-zenbittech@f1f4222e8c41d8e6df2f1570ec7e081cc560843e 🚀

--- static/js/481.53b0e676.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[481],{481:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});var n=r(184),c=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{children:"Home Page"})})}}}]);
-//# sourceMappingURL=481.53b0e676.chunk.js.map
---
+++ static/js/674.53bbda26.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[674],{674:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var n=r(184),a=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("h2",{children:"Catalog"})})}}}]);
+//# sourceMappingURL=674.53bbda26.chunk.js.map
